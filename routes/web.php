3a6fa35cf22d1addde368f0bf6bdff350cf89c99
akmaltheloop-blip <nur.230170136@mhs.unimal.ac.id>
2026-06-08 17:32:04 +0700

    Menambahkan auth route, mengaktifkan form login dari laravel bleze dan membuat form kirim karya

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,15 @@ Route::get('/publikasi', function () {
     return view('publikasi.index');
 })->name('publikasi.index');
 
-Route::get('/kirim-karya', function () {
-    return view('kirim-karya.index');
-})->name('kirim-karya.index');
+//Route::middleware('auth')->group(function (){
+
+    Route::get('/kirim-karya', function () {
+        return view('kirim-karya.index');
+    })->name('kirim-karya.index');
+//});
 
 Route::get('/review', function () {
     return view('review.index');
 })->name('review.index');
+
+require __DIR__.'/auth.php';
