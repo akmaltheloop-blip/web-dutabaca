0867a1aa42f1c79a811adfdbbe13c1203b7297d7
akmaltheloop-blip <nur.230170136@mhs.unimal.ac.id>
2026-06-09 17:28:46 +0700

    menambahkan kolom register(nim,prodi,fakultas) di database, mengedit tampilan regiister dengan menambahkan informasi yang diperlukan

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,17 +2,65 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
+        <!-- Nama -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Nama Lengkap')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+        
+        <!-- NIM -->
+        <div class="mt-4">
+            <x-input-label for="nim" :value="__('NIM')" />
+            <x-text-input id="nim" class="block mt-1 w-full"
+                type="text"
+                name="nim"
+                :value="old('nim')"
+                required />
+            <x-input-error :messages="$errors->get('nim')" class="mt-2" />
+        </div>
+
+        <!-- FAKULTAS -->
+        <div class="mt-4">
+            <x-input-label for="fakultas" :value="__('Fakultas')" />
+            <x-text-input id="fakultas" class="block mt-1 w-full"
+                type="text"
+                name="fakultas"
+                :value="old('fakultas')"
+                required />
+            <x-input-error :messages="$errors->get('fakultas')" class="mt-2" />
+        </div>
+
+        <!-- prodi -->
+        <div class="mt-4">
+            <x-input-label for="prodi" :value="__('Program Studi')" />
+            <x-text-input id="prodi" class="block mt-1 w-full"
+                type="text"
+                name="prodi"
+                :value="old('prodi')"
+                required />
+            <x-input-error :messages="$errors->get('prodi')" class="mt-2" />
+        </div>
+
+        <!-- Username -->
+        <div class="mt-4">
+            <x-input-label for="username" :value="__('Username')" />
+            <x-text-input id="username" class="block mt-1 w-full"
+                type="text"
+                name="username"
+                :value="old('username')"
+                required />
+            <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" 
+            type="email" 
+            name="email" 
+            :value="old('email')" 
+            required />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
