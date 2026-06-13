1c3bf2b30ea9a0d296c885b7da9cb27331b4bb9f
akmaltheloop-blip <nur.230170136@mhs.unimal.ac.id>
2026-06-13 11:03:57 +0700

    mengunci kirim karya agar yang bisa up karya hanya yg sudah punya akun

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,12 @@ Route::get('/publikasi', function () {
     return view('publikasi.index');
 })->name('publikasi.index');
 
-//Route::middleware('auth')->group(function (){
+Route::middleware('auth')->group(function (){
 
     Route::get('/kirim-karya', function () {
         return view('kirim-karya.index');
     })->name('kirim-karya.index');
-//});
+});
 
 Route::get('/review', function () {
     return view('review.index');
